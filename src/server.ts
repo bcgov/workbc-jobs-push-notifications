e0de8eb8d7cd108d513d2295d91eae487d7faa1d
Branko Bajic <branko@mailfence.com>
2022-05-30 14:23:59 -0700

    Update to use hours/min/sec/ms

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,10 @@ cron.schedule("0 8 * * *", async () => {
     console.log("===== START CRON JOB =====")
     const minimumPostedDate = new Date()
     minimumPostedDate.setDate(minimumPostedDate.getDate() - 1) // TODO: fix once AEST api is updated
+    minimumPostedDate.setHours(8)
+    minimumPostedDate.setMinutes(0)
+    minimumPostedDate.setSeconds(0)
+    minimumPostedDate.setMilliseconds(0)
     const usersNotified: string[] = []
 
     try {
