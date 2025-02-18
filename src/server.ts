2f50b774d777bd00c31af57aa13e1328214168cf
Maksym Kononenko <maks@Maksyms-MacBook-Pro.local>
2025-02-18 13:55:47 -0800

    clean up tested solution

--- src/server.ts
+++ src/server.ts
@@ -165,33 +165,10 @@ cron.schedule(
         );
         await Promise.all(
           awaitedJobSearches.map(async ({userId, newJobs}) => {
-            console.log('userId', userId);
             try {
               const firstJobPostingId = newJobs[0]?.data?.[0]?.jobs?.[0]?.JobId;
               const userJobSearch = userIdMapToJobSearch.get(userId)?.[0];
-              console.log('userJobSearch', userJobSearch);
-              console.log('newJobs', newJobs);
               if (userJobSearch) {
-                console.log('sending notification to user', userId);
-                console.log('lang', userJobSearch.language);
-                console.log('token', userJobSearch.token);
-                console.log('platform', userJobSearch.platform);
-                console.log('firstJobPostingId', firstJobPostingId);
-                console.log(
-                  'newJobs.length',
-                  newJobs.length,
-                  !firstJobPostingId,
-                );
-                const data =
-                  newJobs.length > 1 || !firstJobPostingId
-                    ? searchNavigation
-                    : constructJobNavigation(firstJobPostingId);
-                console.log('data', data);
-                console.log(
-                  'sending notification',
-                  process.env.NOTIFICATIONS_API_USER,
-                  process.env.NOTIFICATIONS_API_PASS,
-                );
                 await notificationsApi.post(
                   'messaging/send',
                   {
@@ -225,81 +202,6 @@ cron.schedule(
           }),
         );
       }
-
-      // await db
-      //   .query(
-      //     `
-      //       SELECT js.user_id, js.keyword, js.location, js.language, t.token, t.platform
-      //       FROM job_searches js
-      //       INNER JOIN tokens t ON js.user_id = t.user_id
-      //       WHERE js.user_removed = FALSE
-      //       `,
-      //     [],
-      //   )
-      //   .then(async (jobSearches: QueryResult) => {
-      //     console.log('Checking for new job postings...');
-      //     // for each job search, check if there's new job postings //
-      //     for await (const row of jobSearches.rows) {
-      //       console.log(
-      //         `keyword: ${row.keyword}, location: ${row.location}, user: ${row.user_id}`,
-      //       );
-      //       try {
-      //         const jobsResp = await jobsApi.get('Jobs/SearchJobs', {
-      //           data: {
-      //             jobTitle: row.keyword,
-      //             location: row.location,
-      //             language: row.language,
-      //             minimumPostedDate: minimumPostedDate,
-      //           },
-      //         });
-
-      //         // if there is new job postings, and the user hasn't been sent a push notification yet, send them one //
-      //         if (
-      //           jobsResp.data.count > 0 &&
-      //           !usersNotified.includes(row.user_id)
-      //         ) {
-      //           usersNotified.push(row.user_id);
-      //           try {
-      //             const jobPostingId = jobsResp.data.jobs[0].JobId;
-      //             await notificationsApi.post(
-      //               'Messaging/Send',
-      //               {
-      //                 title:
-      //                   row.language.toUpperCase() === 'EN'
-      //                     ? 'New Jobs Posted'
-      //                     : "Nouvelles offres d'emploi",
-      //                 content:
-      //                   row.language.toUpperCase() === 'EN'
-      //                     ? 'There are new job postings for one or more of your saved job searches!'
-      //                     : 'Il y a de nouvelles offres d’emploi pour une ou plusieurs de vos recherches d’emploi sauvegardées!',
-      //                 token: row.token,
-      //                 platform: row.platform,
-      //                 dryRun: false,
-      //                 data:
-      //                   jobsResp.data.count > 1 || !jobPostingId
-      //                     ? searchNavigation
-      //                     : constructJobNavigation(jobPostingId),
-      //               },
-      //               {
-      //                 auth: {
-      //                   username: process.env.NOTIFICATIONS_API_USER || '',
-      //                   password: process.env.NOTIFICATIONS_API_PASS || '',
-      //                 },
-      //               },
-      //             );
-      //           } catch (e: any) {
-      //             console.log(
-      //               'Error sending notification. Message: ',
-      //               e.message,
-      //             );
-      //           }
-      //         }
-      //       } catch (e: any) {
-      //         console.log('Error searching jobs. Message: ', e.message);
-      //       }
-      //     }
-      //   });
-
       console.log('===== END CRON JOB =====');
     } catch (e: any) {
       console.log(e.message);
